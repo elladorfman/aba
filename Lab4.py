feb0ca8f233638aadd29f8b1c2011c0876566a30
elladorfman <ellardorfman@gmail.com>
2025-02-12 15:15:33 -0500

    Add files via upload

---
+++ Lab4.py
@@ -0,0 +1,21 @@
+
+#Function 1: Fibonacci Numbers
+
+def fibonacci(n):
+        a = 0
+        b = 1
+        if n == 0:
+            return 0
+
+        elif n == 1:
+            return 1
+        elif n == 2:
+            return 1
+        else:
+            for i in range(2, n):
+                c = a + b
+                a = b
+                b = c
+            return b
+
+print(fibonacci(6))
