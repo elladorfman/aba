869d76f79eba1b6944a854918380f994f7c7e177
elladorfman <ellardorfman@gmail.com>
2025-02-12 17:01:38 -0500

    Update Lab4.py

--- Lab4.py
+++ Lab4.py
@@ -19,3 +19,18 @@ def fibonacci(n):
             return b
 
 #print(fibonacci(6))
+
+#Function 2: Prime Numbers
+
+def is_prime(num):
+    if num > 1:
+        for i in range(2, (num // 2) + 1):
+            if (num % i) == 0:
+                return False
+                break
+        else:
+            return True
+    else:
+        return False
+
+#print(is_prime(1))
